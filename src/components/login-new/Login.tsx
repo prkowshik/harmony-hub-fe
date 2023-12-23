d5e813ca6a697fd84be56c6df449de060e137df5
Pavana345 <82650697+Pavana345@users.noreply.github.com>
2023-12-23 13:33:10 +0530

    updated login page

--- src/components/login-new/Login.tsx
+++ src/components/login-new/Login.tsx
@@ -10,6 +10,41 @@ function Login(){
             <p className='Ellipse1'></p>
             <p className='Ellipse2'></p>
             <div className='login-frame1'>
+                <div className='login-frame2'>
+                    <div className='login-frame3'>
+                    <div className='upper-section'>
+                        <div className='login-text-div'><p className='login-text'>Login</p></div>
+                        <div className='login-text-div'><p className='sub-text'>Glad you're back.!</p></div>
+                        {/* <div className='credentials'>
+                            <div className='username'><p className='username-text'>Username</p></div>
+                        </div> */}
+                        <form className='credentials'>
+                        <div className='username'><p className='username-text'>Username</p></div>
+                        <div className='password'>
+                            <p className='password-text'>Password</p>
+                            <img src="/closedeye.svg"></img>
+                            
+                        </div>
+                        <div className='reminder-div'>
+                            <input className="checkbox-div" type="checkbox" />
+                            <p className='remember-me-text'>Remember me</p>
+                        </div>
+                        <div className='login-bt-fp'>
+                            <input className="login-bt-div"  type="button" value="Login" />
+                            <span className='fp-text'>Forgot password ?</span>
+                        </div>
+
+                            
+                        </form>
+                        <img src="/or-line.svg" width="305"></img>
+                        <ul>
+                           <li><img src="/google-icon.svg"></img></li> 
+                           <li><img src="/fb-icon.svg"></img></li> 
+                           <li><img src="/github.svg"></img></li> 
+                        </ul>
+                    </div>
+                    </div>
+                </div>
 
             </div>
             
